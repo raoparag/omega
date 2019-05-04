7836cc17fb2f91498034098f4697f123ac107bd2
parag <raoparag@gmail.com>
2019-05-04 17:30:11 +0800

    added booking master

---
+++ modules/web/src/com/srt/omega/web/dashboards/BookingMaster.java
@@ -0,0 +1,10 @@
+package com.srt.omega.web.dashboards;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("omega_BookingMaster")
+@UiDescriptor("BookingMaster.xml")
+public class BookingMaster extends Screen {
+}
